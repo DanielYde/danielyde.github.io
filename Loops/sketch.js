04c75498f6c75d08392f8fa3a924c174f87f55a0
Daniel <daniel.al.yde@gmail.com>
2020-11-06 17:48:38 +0100

    k

---
+++ Loops/sketch.js
@@ -0,0 +1,30 @@
+  function Countdown() {
+      let i = 11;
+      while (i>0) {
+        i = i - 1;
+        console.log(i);
+    }
+  }      
+
+    function Plus() {
+      let i = 0
+      let n = 0
+      while (i<210) {
+        i = i + n + 1
+        n = n + 1
+        console.log(i)
+      }
+    }
+
+    function Tabeller() {
+      
+
+    }
+
+    function Ligninger() {
+
+    }
+
+
+
+  
