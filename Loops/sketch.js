44b9abe59f4af5df662cb3aacba55ab43e2031e2
DanielYde <daniel.al.yde@gmail.com>
2020-11-06 19:17:11 +0100

    Made number 3

--- Loops/sketch.js
+++ Loops/sketch.js
@@ -17,7 +17,70 @@
     }
 
     function Tabeller() {
-      
+
+      let totabellen = 0;
+      console.log("2-tabellen")
+      while (totabellen < 20) {
+        totabellen = totabellen + 2;
+        console.log(totabellen)
+
+      }
+
+      let tretabellen = 0;
+      console.log("3-tabellen")
+      while (tretabellen < 30) {
+        tretabellen = tretabellen + 3
+        console.log(tretabellen)
+      }
+
+      let firetabellen = 0;
+      console.log("4-tabellen")
+      while (firetabellen < 40) {
+        firetabellen = firetabellen + 4
+        console.log(firetabellen)
+      }
+
+      let femtabellen = 0;
+      console.log("5-tabellen")
+      while (femtabellen < 50) {
+        femtabellen = femtabellen + 5
+        console.log(femtabellen)
+      }
+
+      let sekstabellen = 0;
+      console.log("6-tabellen")
+      while (sekstabellen < 60) {
+        sekstabellen = sekstabellen + 6
+        console.log(sekstabellen)
+      }
+
+      let syvtabellen = 0;
+      console.log("7-tabellen")
+      while (syvtabellen < 70) {
+        syvtabellen = syvtabellen + 7
+        console.log(syvtabellen)
+      }
+
+      let ottetabellen = 0;
+      console.log("8-tabellen")
+      while (ottetabellen < 80) {
+        ottetabellen = ottetabellen + 8
+        console.log(ottetabellen)
+      }
+
+      let nitabellen = 0;
+      console.log("9-tabellen")
+      while (nitabellen < 90) {
+        nitabellen = nitabellen + 9
+        console.log(nitabellen)
+      }
+
+      let titabellen = 0;
+      console.log("10-tabellen")
+      while (titabellen < 100) {
+        titabellen = titabellen + 10
+        console.log(titabellen)
+      }
 
     }
 
